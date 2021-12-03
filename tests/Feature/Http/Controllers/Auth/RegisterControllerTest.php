836513858e9ae6a1a5ef52fe9d103d7ca0dea16e
Yoshinori Shimoda <ys@platinum.local>
2021-12-04 03:03:59 +0900

    RegisterControllerのテスト、アノテーション記述

---
+++ tests/Feature/Http/Controllers/Auth/RegisterControllerTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\Auth;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class RegisterControllerTest extends TestCase
+{
+
+    /** @test */
+    public function 認証機能でアカウントが作成できること()
+    {
+        $response = $this->get(route('register'));
+        $response->assertViewIs('auth.register');
+    }
+}
