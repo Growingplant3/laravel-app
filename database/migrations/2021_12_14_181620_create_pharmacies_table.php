50c6b011ffed6e384ec3ea8e61cb22cce502c596
Yoshinori Shimoda <ys@platinum.local>
2021-12-15 03:35:42 +0900

    暫定でPharamcyのカラム設定、認証関係は後で

---
+++ database/migrations/2021_12_14_181620_create_pharmacies_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePharmaciesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pharmacies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('postcode');
+            $table->string('address');
+            $table->string('normal_telephone_number');
+            $table->string('emergency_telephone_number');
+            $table->text('note');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pharmacies');
+    }
+}
