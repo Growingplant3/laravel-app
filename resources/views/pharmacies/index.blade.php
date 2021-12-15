63d38ee111b7d8014f53dc9309d939fe103eefec
Yoshinori Shimoda <ys@platinum.local>
2021-12-16 04:10:59 +0900

    viewで変数取得、一旦テーブル表示で

--- resources/views/pharmacies/index.blade.php
+++ resources/views/pharmacies/index.blade.php
@@ -1,1 +1,16 @@
-<h1>pharmacy</h1>
+<h1>薬局一覧</h1>
+
+@foreach($pharmacies as $key => $pharmacy)
+    <table rules='all' bgcolor='aqua'>
+        <tr>
+            <th>{{ $key }}ばんめ</th>
+            <td>{{ $pharmacy->name }}</td>
+            <td>{{ $pharmacy->email }}</td>
+            <td>{{ $pharmacy->postcode }}</td>
+            <td>{{ $pharmacy->address }}</td>
+            <td>{{ $pharmacy->normal_telephone_number }}</td>
+            <td>{{ $pharmacy->emergency_telephone_number }}</td>
+        </tr>
+        <br>
+    </table>
+@endforeach
