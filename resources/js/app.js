039286c26b19235e120c4cee2e656a36944a1c5a
Yoshinori Shimoda <ys@platinum.local>
2021-12-21 01:55:21 +0900

    laravel-mixのインストール

--- resources/js/app.js
+++ resources/js/app.js
@@ -18,7 +18,8 @@ window.Vue = require('vue').default;
 
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
-
+import { createApp } from 'vue/dist/vue.esm-bundler.js';
+window.createApp = createApp;
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
 /**
