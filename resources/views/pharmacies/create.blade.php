167708cd1f871c737569aeea3d66f38af6eb0717
Yoshinori Shimoda <ys@platinum.local>
2021-12-22 02:17:32 +0900

    エラー起きないことを確認/bootstrapの適応を確認

--- resources/views/pharmacies/create.blade.php
+++ resources/views/pharmacies/create.blade.php
@@ -1,2 +1,17 @@
-<h1>薬局新規作成</h1>
+@extends('layouts.app')
 
+@section('content')
+	<h1>薬局新規作成</h1>
+
+	{{ Form::model([$pharmacy, 'route' => 'pharmacies.store', 'method' => 'post',])}}
+
+		<div>
+			{{ Form::label('name', '薬局名') }}
+			{{ Form::text('name', 'hello', ['class' => 'form-control'])}}
+		</div>
+
+		{{ Form::submit('登録する', ['class' => 'btn btn-primary']) }}
+	{{ Form::close() }}
+
+	<button type="submit" class="btn btn-primary mb-3">Confirm identity</button>
+@endsection
